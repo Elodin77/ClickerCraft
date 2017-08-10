7bdb7da0e3b8a334e0e8a9afec7fccfa581628cc
TheNumnut <woofless77@gmail.com>
2017-08-10 16:41:46 +0800

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
   $file = fopen("log.txt", "w") or die("Unable to open file!");
-  $message = $_POST['message']; // incoming message
-  fwrite($file, $message);
+  
+  fwrite($file, "HI");
 ?>
