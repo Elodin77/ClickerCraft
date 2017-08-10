b6a6ad880f350d82dc3bfb4950472016f410619a
TheNumnut <woofless77@gmail.com>
2017-08-10 16:21:45 +0800

    Update script.php

--- script.php
+++ script.php
@@ -1,5 +1,5 @@
 <?php
-  $log_file_name = 'mylog.log'; // Change to the log file name
+  $log_file_name = 'log.txt'; // Change to the log file name
   $message = $_POST['message']; // incoming message
   file_put_contents($log_file_name, $message, FILE_APPEND);
   header('Location: /'); // redirect back to the main site
